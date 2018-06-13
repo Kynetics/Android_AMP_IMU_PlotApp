d2cef886fa6efef6368b175352ebdeb7688ec173
Marta Todeschini <marta.todeschini@kynetics.com>
2018-06-13 09:26:32 +0200

    Updated the jni to make open(), ioctl() and close() calls to get the statistics

--- amp-sensors/src/main/jni/native-lib.c
+++ amp-sensors/src/main/jni/native-lib.c
@@ -29,13 +29,15 @@
 
 #define RPMSG_CREATE_EPT_IOCTL  _IOW(0xb5, 0x1, struct rpmsg_endpoint_info)
 #define RPMSG_DESTROY_EPT_IOCTL _IO(0xb5, 0x2)
-#define EPT_SRC     0x401
+#define EPT_SRC_IMU     0x401
+#define EPT_SRC_STAT     0x402
 #define EPT_DST     0x0
 #define DEV_CREATE_TIMEOUT_SEC  5
 
 int sleepTime = 500000;
 const char *ctrlDev = "/dev/rpmsg_ctrl0";
 const char *eptDev = "/dev/rpmsg0";
+const char *eptStat = "/dev/rpmsg1";
 
 struct rpmsg_endpoint_info {
     char name[32];
@@ -43,35 +45,52 @@ struct rpmsg_endpoint_info {
     uint32_t dst;
 };
 
-struct rpmsg_endpoint_info ep = {
+struct rpmsg_endpoint_info ep_imu = {
         .name   = "rpmsg-openamp-demo-channel",
-        .src    = EPT_SRC,
+        .src    = EPT_SRC_IMU,
         .dst    = EPT_DST,
+};
+
+struct rpmsg_endpoint_info ep_stat = {
+        .name = "rpmsg-openamp-demo-channel",
+        .src = EPT_SRC_STAT,
+        .dst = EPT_DST,
 };
 
 JNIEXPORT jobject JNICALL
 Java_com_kynetics_ampsensors_device_DeviceManager_openDeviceNative(JNIEnv *env, jobject instance) {
 
     time_t t_end, t_start;
-    jstring retstr = (*env)->NewStringUTF(env, eptDev);
-    int fd_ctrldev, ret;
+
+    /*Paths to return*/
+    jstring retstrImu = (*env)->NewStringUTF(env, eptDev);
+    jstring retstrStat = (*env)->NewStringUTF(env, eptStat);
+
+    int fd_ctrldev, ret_imu, ret_stat;
     jclass jcls = (*env)->FindClass(env,
                                     "com/kynetics/ampsensors/device/DeviceManager$DeviceDescriptor");
-    jmethodID cstr = (*env)->GetMethodID(env, jcls, "<init>", "(ILjava/lang/String;)V");
+    jmethodID cstr = (*env)->GetMethodID(env, jcls, "<init>", "(ILjava/lang/String;Ljava/lang/String;)V");
 
 
     /* Open controller device */
      fd_ctrldev = open(ctrlDev, O_RDONLY);
-
     if (fd_ctrldev < 0) {
         __android_log_print(ANDROID_LOG_INFO, "openDeviceNative", "Error opening controller device: %s \n",strerror(errno));
         return NULL;
     }
 
     /* Create endpoint device */
-    ret = ioctl(fd_ctrldev, RPMSG_CREATE_EPT_IOCTL, &ep);
-    if (ret < 0) {
+    ret_imu = ioctl(fd_ctrldev, RPMSG_CREATE_EPT_IOCTL, &ep_imu);
+    if (ret_imu < 0) {
         __android_log_print(ANDROID_LOG_INFO, "openDeviceNative", "Error creating endpoint device: %s \n",strerror(errno));
+        close(fd_ctrldev);
+        return NULL;
+    }
+
+    /* Create endpoint device for statistics*/
+    ret_stat = ioctl(fd_ctrldev, RPMSG_CREATE_EPT_IOCTL, &ep_stat);
+    if (ret_stat < 0) {
+        __android_log_print(ANDROID_LOG_INFO, "openDeviceNative", "Error creating endpoint device for statistics: %s \n",strerror(errno));
         close(fd_ctrldev);
         return NULL;
     }
@@ -87,29 +106,52 @@ Java_com_kynetics_ampsensors_device_DeviceManager_openDeviceNative(JNIEnv *env, 
         }
         usleep(sleepTime);
     }
+    /* Check endpoint device creation */
+    t_start = time(0);
+    while (access(eptStat, 0)) {
+        t_end = time(0);
+        if ((t_end - t_start) > DEV_CREATE_TIMEOUT_SEC) {
+            __android_log_print(ANDROID_LOG_INFO, "openDeviceNative", "Device is taking too long to be created, aborting...\n");
+            close(fd_ctrldev);
+            break;
+        }
+        usleep(sleepTime);
+    }
 
     usleep(sleepTime);
-    jobject obj = (*env)->NewObject(env, jcls, cstr, fd_ctrldev, retstr);
+    jobject obj = (*env)->NewObject(env, jcls, cstr, fd_ctrldev, retstrImu, retstrStat);
     return obj;
 }
 
 JNIEXPORT void JNICALL
 Java_com_kynetics_ampsensors_device_DeviceManager_closeDeviceNative(JNIEnv *env, jobject instance,
                                                                     jint fd) {
-    int fd_ept, ret;
+    int fd_ept_imu, fd_ept_stat, ret_imu, ret_stat;
 
-    fd_ept = open(eptDev, O_RDONLY);
-    if(fd_ept <0) {
+    fd_ept_imu = open(eptDev, O_RDONLY);
+    if(fd_ept_imu <0) {
         __android_log_print(ANDROID_LOG_INFO, "closeDeviceNative", "Error opening endpoint device: %s \n",strerror(errno));
     }
 
+    fd_ept_stat = open(eptStat, O_RDONLY);
+    if(fd_ept_stat <0) {
+        __android_log_print(ANDROID_LOG_INFO, "closeDeviceNative", "Error opening endpoint device statistics: %s \n",strerror(errno));
+    }
+
+
     /* Destroy endpoint */
-    ret = ioctl(fd_ept, RPMSG_DESTROY_EPT_IOCTL);
-    if (ret < 0) {
+    ret_imu = ioctl(fd_ept_imu, RPMSG_DESTROY_EPT_IOCTL);
+    if (ret_imu < 0) {
         __android_log_print(ANDROID_LOG_INFO, "closeDeviceNative", "Error destroying endpoint: %s \n",strerror(errno));
         }
 
+    ret_stat = ioctl(fd_ept_stat, RPMSG_DESTROY_EPT_IOCTL);
+    if (ret_stat < 0) {
+        __android_log_print(ANDROID_LOG_INFO, "closeDeviceNative", "Error destroying endpoint: %s \n",strerror(errno));
+    }
+
     usleep(sleepTime);
-    close(fd_ept);
+    close(fd_ept_imu);
+    close(fd_ept_stat);
     close(fd);
 }
